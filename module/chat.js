3c5346e8cb1d498d02cec9abf16b8451d8b7ae53
Stefouch <47865544+Stefouch@users.noreply.github.com>
2021-08-13 12:03:16 +0200

    feat(contextMenu): applyDamage to selected tokens

--- module/chat.js
+++ module/chat.js
@@ -1,6 +1,8 @@
 import ActorT2K from './actor/actor.js';
 import ItemT2K from './item/item.js';
-import { rollPush } from './dice.js';
+import T2KDialog from './dialog.js';
+import { getRollingActor, rollPush } from './dice.js';
+import { T2K4E } from './config.js';
 
 /**
  * Adds Event Listeners to the Chat log.
@@ -71,9 +73,81 @@ export function getChatCardActor(card) {
  * Adds a context menu (right-clic) to Chat messages.
  * @param {Object} html DOM
  * @param {Object} options Options
+ * @link https://www.youtube.com/watch?v=uBC5DSci0NI&list=PLFV9z59nkHDccUbRXVt623UdloPTclIrz&index=7
  */
 export function addChatMessageContextOptions(html, options) {
   // TODO: See Part 6, 6:55
+  // Allows only this menu option if we have selected some tokens
+  // & the message contains some damage.
+  const canDefend = li => canvas.tokens.controlled.length && li.find('.dice-info.roll-trauma').length;
+  options.push(
+    {
+      name: game.i18n.localize('T2K4E.Chat.Actions.ApplyDamage'),
+      icon: T2K4E.Icons.buttons.attack,
+      condition: canDefend,
+      callback: li => _applyDamage(li[0]),
+    },
+  );
+  return options;
+}
+
+/* ------------------------------------------- */
+
+async function _applyDamage(messageElem) {
+  const messageId = messageElem.dataset.messageId;
+  const message = game.messages.get(messageId);
+  /** @type {import('../lib/yzur.js').YearZeroRoll} */
+  const roll = message.roll;
+
+  // Gets the weapon.
+  const actorId = roll.data.actorId;
+  const tokenKey = roll.data.tokenId;
+  const actor = getRollingActor({ actorId, tokenKey });
+  const itemId = roll.data.itemId;
+  const item = actor ? actor.items.get(itemId) : game.items.get(itemId);
+
+  // Prepares the attack's data.
+  let attackData = foundry.utils.duplicate(item.data.data);
+  if (actor && item.hasAmmo) {
+    const ammo = actor.items.get(item.data.data.mag.target);
+    if (ammo && ammo.data.data.override) {
+      const ammoData = foundry.utils.duplicate(ammo.data.data);
+      attackData = foundry.utils.mergeObject(attackData, ammoData);
+    }
+  }
+  const loc = roll.bestHitLocation;
+  if (loc > 0) attackData.location = T2K4E.hitLocs[loc - 1];
+
+  // Gets the selected tokens.
+  const defenders = canvas.tokens.controlled;
+  for (const defender of defenders) {
+    const s = roll.baseSuccessQty;
+    let damage = s > 0 ? attackData.damage + 1 * (s - 1) : 0;
+    const isGM = game.user.isGM;
+    let hitCount = message.getFlag('t2k4e', 'hitCountLeft') ?? roll.hitCount;
+    attackData.cover = defender.actor.cover;
+    let barrier = 0;
+    if (attackData.cover === 'fullCover') barrier = 2;
+    else if (attackData.cover === 'partialCover') barrier = 1;
+    if (isGM || hitCount) {
+      const opts = await T2KDialog.chooseDamage({
+        damage,
+        hitCount,
+        location: attackData.location,
+        barrier,
+        isGM,
+      });
+      if (!opts.cancelled) {
+        damage = opts.damage + opts.hitCount;
+        if (opts.barrier) attackData.barrier = opts.barrier;
+        if (opts.hitCount) {
+          hitCount = Math.max(0, hitCount - opts.hitCount);
+          await message.setFlag('t2k4e', 'hitCountLeft', hitCount);
+        }
+      }
+    }
+    await defender.actor.applyDamage(damage, attackData);
+  }
 }
 
 /* ------------------------------------------- */
